2699d0603ab47a5257835b400dec385670937dee
gusbdie <gustavobdetrich@gmail.com>
2025-02-18 19:59:28 -0300

    Criando o modelo de botão baseado na calculadora da apple

--- src/br/com/gustavo/calc/visao/Botao.java
+++ src/br/com/gustavo/calc/visao/Botao.java
@@ -1,7 +1,19 @@
 package br.com.gustavo.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class Botao extends JButton{
 
+    public Botao(String texto, Color cor){
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setFont(new Font("courier", Font.PLAIN, 25));
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
 }

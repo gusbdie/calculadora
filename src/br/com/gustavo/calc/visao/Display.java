f5db8e206ddcac023d85e842816e9281c3d471b3
gusbdie <gustavobdetrich@gmail.com>
2025-02-23 17:12:41 -0300

    O método valor alterado serve para que cada vez que cliquemos em um botão, o valor no Display seja mudado

--- src/br/com/gustavo/calc/visao/Display.java
+++ src/br/com/gustavo/calc/visao/Display.java
@@ -7,17 +7,26 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Display extends JPanel{
+import br.com.gustavo.calc.modelo.*;
+
+public class Display extends JPanel implements MemoriaObservador{
 
     private JLabel label = new JLabel();
 
     public Display(){
+
+        Memoria.getInstancia().adicionarObservador(this);
         setBackground(new Color(46, 49, 50));
-        label = new JLabel("1234,56");
+        label = new JLabel(Memoria.getInstancia().getTextoAtual());
         label.setForeground(Color.WHITE);
         label.setFont(new Font("courier", Font. PLAIN, 30));
 
         setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
         add(label);
+    }
+
+    @Override
+    public void valorAlterado(String novoValor) {
+        label.setText(novoValor);
     }
 }

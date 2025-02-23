1b11251335df9877ec06e2a487fa425e83fd75cc
gusbdie <gustavobdetrich@gmail.com>
2025-02-23 17:00:38 -0300

    Agora os botões são capazes de fazer alguma ação. Ao serem clicados eles emitiram alguma resposta baseado no método processarComando

--- src/br/com/gustavo/calc/visao/Teclado.java
+++ src/br/com/gustavo/calc/visao/Teclado.java
@@ -1,13 +1,17 @@
 package br.com.gustavo.calc.visao;
 
 import java.awt.Color;
-import java.awt.GridLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Teclado extends JPanel{
+import br.com.gustavo.calc.modelo.Memoria;
+
+public class Teclado extends JPanel implements ActionListener{
 
     private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
     private final Color COR_CINZA_CLARO = new Color(99, 99, 99);
@@ -64,6 +68,14 @@ public class Teclado extends JPanel{
         c.gridx = x;
         c.gridy = y;
         Botao botao = new Botao(texto, cor);
+        botao.addActionListener(this);
         add(botao, c);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() instanceof JButton){
+            JButton botao = (JButton) e.getSource();
+            Memoria.getInstancia().processarComando(botao.getText());
+        }
     }
 }

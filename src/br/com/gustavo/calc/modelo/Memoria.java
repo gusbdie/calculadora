88b3e6f5d556118aed3a4884b3dc020d1acc8be3
gusbdie <gustavobdetrich@gmail.com>
2025-02-23 21:25:20 -0300

    Fazendo toda lógica matemática da calculadora nos métodos processarComando e obterResultadoOperacao

--- src/br/com/gustavo/calc/modelo/Memoria.java
+++ src/br/com/gustavo/calc/modelo/Memoria.java
@@ -6,17 +6,22 @@ import java.util.List;
 public class Memoria {
 
     public enum TipoComando {
-        ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA;
+        ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA, INVERTER;
     }
 
     private static final Memoria instancia = new Memoria();
     //com o final, nem adiantará por um set pois não será possível 
     //alterar o valor
-
-    private String textoAtual = "";
-
     private final List<MemoriaObservador> observadores =
         new ArrayList<>();
+
+    private TipoComando ultimaOperacao = null;
+    //armazena qual variável será executada na operação
+    private boolean substituir = false;
+    //ele que vai ser responsável pela troca de valor mostrado no display
+    private String textoAtual = "";
+    private String textoBuffer = "";
+    //variavél responsável por armazenar os números do cálculo
 
     private Memoria(){
 
@@ -40,13 +45,61 @@ public class Memoria {
         //é possível criar ENUM's como variáveis
         TipoComando tipoComando = detectarTipoComando(texto);
 
-        if("AC".equals(texto)){
+        if(tipoComando == null){
+            return;
+        }else if(tipoComando == TipoComando.ZERAR){
             textoAtual = "";
-        }else {
-            textoAtual += texto;
+            textoBuffer = "";
+            substituir = false;
+            ultimaOperacao = null;
+        }else if(tipoComando == TipoComando.NUMERO 
+        || tipoComando == TipoComando.VIRGULA){
+            textoAtual = substituir ? texto : textoAtual + texto;
+            substituir = false;
+        }else if(tipoComando == TipoComando.INVERTER && textoAtual.contains("-")){
+            textoAtual = textoAtual.substring(1);
+        }else if(tipoComando == TipoComando.INVERTER && !textoAtual.contains("-")){
+            textoAtual = "-" + textoAtual;
+        }
+        else{//aqui vai ficar os cálculos matemáticos
+            substituir = true;
+            textoAtual = obterResultadoOperacao();
+            textoBuffer = textoAtual;
+            ultimaOperacao = tipoComando;
         }
 
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private String obterResultadoOperacao(){
+        if(ultimaOperacao == null 
+        || ultimaOperacao == TipoComando.IGUAL){
+            return textoAtual;
+        }
+
+        double numeroBuffer = 
+        Double.parseDouble(textoBuffer.replace(",", "."));
+        double numeroAtual = 
+        Double.parseDouble(textoAtual.replace(",", "."));
+        
+        double resultado = 0;
+
+        if(ultimaOperacao == TipoComando.SOMA){
+            resultado = numeroBuffer + numeroAtual;
+        }else if(ultimaOperacao == TipoComando.SUB){
+            resultado = numeroBuffer - numeroAtual;
+        }else if(ultimaOperacao == TipoComando.MULT){
+            resultado = numeroBuffer * numeroAtual;
+        }else if(ultimaOperacao == TipoComando.DIV){
+            resultado = numeroBuffer / numeroAtual;
+        }
+
+        String resultadoString = 
+        Double.toString(resultado).replace(".", ",");
+        boolean inteiro = resultadoString.endsWith(",0");
+        //muito interessante esse endsWith, poupa muito trabalho
+        return inteiro ? resultadoString.replace(",0", "") 
+        : resultadoString;
     }
 
     private TipoComando detectarTipoComando(String texto){
@@ -72,11 +125,12 @@ public class Memoria {
                 return TipoComando.SUB;
             }else if("=".equals(texto)){
                 return TipoComando.IGUAL;
-            }else if(",".equals(texto)){
+            }else if(",".equals(texto) && !textoAtual.contains(",")){
                 return TipoComando.VIRGULA;
+            }else if("+-".equals(texto)){
+                return TipoComando.INVERTER;
             }
         }
-
         return null;
     }
 }
